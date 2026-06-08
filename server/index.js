ba502fb628438666ab085fbc447bc7cb8de74bad
shelfmerch <shelfmerch@gmail.com>
2026-06-08 13:55:15 +0530

    cors origin added

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ app.use(cors(
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "http://82.29.160.45:5174",
+        "http://82.29.160.45:5173"
     ],
     credentials: true,
   }
